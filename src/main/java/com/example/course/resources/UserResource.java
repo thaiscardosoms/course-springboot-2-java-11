1ddd8ce1a3c2f3cb6be85febc817bcff14765d81
Thais Cardoso <thaiscardoso.ms@gmail.com>
2020-03-18 22:20:12 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.course.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.example.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@GetMapping
 	public ResponseEntity<User> findAll(){
 		
 	}
